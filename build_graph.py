27e0dc732266164e1d233f710fc16cee4ff13dd7
Stefan van der Walt <stefanv@berkeley.edu>
2018-03-19 22:33:41 -0700

    Complete data matrix; plot

--- build_graph.py
+++ build_graph.py
@@ -7,26 +7,37 @@ df = pd.read_csv('data/Preworkshop questionaire.csv')
 def column_mapper(column):
     return re.search('\[(.*)\]', column).group(1)
 
-people = df[[column for column in df.columns if column.startswith('How do you know')]]
-people = people.rename(columns=column_mapper)
+about_person = df[[column for column in df.columns if column.startswith('How do you know')]]
+about_person = about_person.rename(columns=column_mapper)
 
-respondent_idx = (people == 'It\'s me').values.argmax(axis=1)
-respondents = people.columns[respondent_idx]
+respondent_idx = (about_person == 'It\'s me').values.argmax(axis=1)
+respondents = about_person.columns[respondent_idx]
 
-N = len(people.columns)
+N = len(about_person.columns)
 M = len(respondents)
 fields = ('Collaborate', 'Personally communicate', 'Don\'t know them')
-collaborations = np.zeros((M, N, len(fields)))
+graphs = np.zeros((M, N, len(fields)))
 
 print('Row labels:', respondents)
-print('Column labels:', people.columns)
+print('Column labels:', about_person.columns)
 
-for person in people:
+for n, person in enumerate(about_person):
     print(f'person == {person}')
-    responses = people[person]
+    responses = about_person[person]
 
-    for (index, field) in enumerate(fields):
-        for response in responses:
-            pass
+    for (f, field) in enumerate(fields):
+        for r, response in enumerate(responses):
+            if (field in response):
+                graphs[r, n, f] = 1
 
-print(collaborations.shape)
+import matplotlib.pyplot as plt
+f, axes = plt.subplots(1, len(fields))
+for f, field in enumerate(fields):
+    axes[f].imshow(graphs[..., f], cmap='gray', vmin=0, vmax=1)
+    axes[f].set_title(field)
+    axes[f].set_xticks(np.arange(N))
+    axes[f].set_xticklabels(about_person.columns, rotation=45, horizontalalignment='right')
+    axes[f].set_yticks(np.arange(M))
+    axes[f].set_yticklabels(respondents, rotation=45, horizontalalignment='right')
+
+plt.show()
